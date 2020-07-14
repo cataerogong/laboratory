dcc95b01ccb674d940e2e0e1e16fd71ce738f2ba
CataeroGong <cataerogong@gmail.com>
2020-07-14 21:39:12 +0800

    typer

--- typer___#py#/echo_md.cmd
+++ typer___#py#/echo_md.cmd
@@ -3,6 +3,7 @@ echo ## run
 echo.
 echo ### main.py
 echo.
+echo ```console
 call echo_md_run.cmd "python main.py --help"
 call echo_md_run.cmd "python main.py main --help"
 call echo_md_run.cmd "python main.py sub --help"
@@ -13,14 +14,21 @@ call echo_md_run.cmd "python main.py main sth"
 call echo_md_run.cmd "python main.py sub sth"
 call echo_md_run.cmd "python main.py sub2 cmd1 sth"
 call echo_md_run.cmd "python main.py sub2 cmd2 sth"
+echo ```
+echo.
 echo ### sub.py
 echo.
+echo ```console
 call echo_md_run.cmd "python sub.py --help"
 call echo_md_run.cmd "python sub.py sth"
+echo ```
+echo.
 echo ### sub2.py
 echo.
+echo ```console
 call echo_md_run.cmd "python sub2.py --help"
 call echo_md_run.cmd "python sub2.py cmd1 --help"
 call echo_md_run.cmd "python sub2.py cmd2 --help"
 call echo_md_run.cmd "python sub2.py cmd1 sth"
 call echo_md_run.cmd "python sub2.py cmd2 sth"
+echo ```
--- typer___#py#/echo_md_run.cmd
+++ typer___#py#/echo_md_run.cmd
@@ -1,6 +1,4 @@
 @echo off
-echo `$ %~1`
-echo ```
+echo $ %~1
 %~1
-echo ```
 echo.
